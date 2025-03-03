d7d5a13193529f90958a056798bcfe943fca8741
Erik Velf <velf.erik@protonmail.com>
2025-03-03 22:23:07 +0100

    Add authentication via strategies using @clerk useOAuth

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,55 @@
-import { Text, View } from "react-native";
+import * as WebBrowser from "expo-web-browser";
+import {
+  UseOAuthFlowParams,
+  useSSO,
+  StartSSOFlowParams,
+  useOAuth,
+} from "@clerk/clerk-expo";
+import { useCallback } from "react";
+import { Button, Text, View } from "react-native";
 
 export default function Index() {
+  const { startOAuthFlow } = useOAuth({ strategy: "oauth_apple" });
+  const { startOAuthFlow: googleAuth } = useOAuth({ strategy: "oauth_google" });
+  // const { startSSOFlow } = useSSO();
+
+  const handleAppleOAuth = async () => {
+    try {
+      // Important for updating our current session
+      const { createdSessionId, setActive } = await startOAuthFlow();
+      console.log("handleAppleOAuth ~ createSessionId", createdSessionId);
+
+      // locally set the user as active using the current sessionId
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      }
+    } catch (error) {
+      // otherwise catch error
+      console.log(error);
+    }
+  };
+
+  const handleGoogleOAuth = async () => {
+    try {
+      // Important for updating our current session
+      const { createdSessionId, setActive } = await googleAuth();
+      console.log("handleAppleOAuth ~ createSessionId", createdSessionId);
+
+      // locally set the user as active using the current sessionId
+      if (createdSessionId) {
+        setActive!({ session: createdSessionId });
+      }
+    } catch (error) {
+      // otherwise catch error
+      console.log(error);
+    }
+  };
+
+  const openLink = async () => {
+    // like some FAQ link
+    WebBrowser.openBrowserAsync("https://galaxies.dev");
+  };
+
   return (
     <View
       style={{
