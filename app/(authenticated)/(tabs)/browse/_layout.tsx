b68c0b47e26433aaf75a9e3aebe70011832a0cd8
Erik Velf <velf.erik@protonmail.com>
2025-03-11 21:39:40 +0100

    Add blueprint layout and index page files for tabs

---
+++ app/(authenticated)/(tabs)/browse/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from "expo-router";
+
+const Layout = () => {
+	return (
+		// Stack makes a chain of screens like screen 1 > 2 > 3 and so on with functionallity to come back to the previous screens (LIFO stack)
+		<Stack />
+	);
+};
+
+export default Layout;
