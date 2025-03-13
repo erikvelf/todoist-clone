2094f52e156055ea14e65d2083293b54504d2b07
Erik Velf <velf.erik@protonmail.com>
2025-03-13 21:15:19 +0100

    Update layout for tabs to include icons for different screens

--- app/(authenticated)/(tabs)/_layout.tsx
+++ app/(authenticated)/(tabs)/_layout.tsx
@@ -1,14 +1,54 @@
 import { Tabs } from "@/components/Tabs";
 
 const TabLayout = () => {
+	// sfSymbol is a lib of icons from an installed icons package
 	return (
 		<Tabs>
 			<Tabs.Screen
 				name="index"
 				options={{
 					title: "Home",
-					// sfSymbol is a lib of icons from an installed icons package
-					tabBarIcon: () => ({ sfSymbol: "house" }),
+					tabBarIcon: ({ focused }) => ({
+						sfSymbol: focused ? "house.fill" : "house",
+					}),
+				}}
+			/>
+			<Tabs.Screen
+				name="today"
+				options={{
+					title: "Today",
+					tabBarIcon: ({ focused }) => ({
+						sfSymbol: focused ? "calendar.circle.fill" : "calendar.circle.fill",
+					}),
+				}}
+			/>
+			<Tabs.Screen
+				name="upcoming"
+				options={{
+					title: "Upcoming",
+					tabBarIcon: () => ({
+						sfSymbol: "calendar",
+					}),
+				}}
+			/>
+
+			<Tabs.Screen
+				name="search"
+				options={{
+					title: "Search",
+					tabBarIcon: ({ focused }) => ({
+						sfSymbol: focused ? "text.magnifyingglass" : "magnifyingglass",
+					}),
+				}}
+			/>
+
+			<Tabs.Screen
+				name="browse"
+				options={{
+					title: "Browse",
+					tabBarIcon: ({ focused }) => ({
+						sfSymbol: focused ? "doc.text.image.fill" : "doc.text.image",
+					}),
 				}}
 			/>
 		</Tabs>
