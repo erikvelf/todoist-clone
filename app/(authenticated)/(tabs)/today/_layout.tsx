73c30b9c052787c8dfa1f7630fe1c585ebab6bbc
Erik Velf <velf.erik@protonmail.com>
2025-03-31 21:38:57 +0200

    Add Stack.Screen options for today tab

--- app/(authenticated)/(tabs)/today/_layout.tsx
+++ app/(authenticated)/(tabs)/today/_layout.tsx
@@ -1,10 +1,24 @@
+import { Colors } from "@/constants/Colors";
 import { Stack } from "expo-router";
 
 const Layout = () => {
-	return (
-		// Stack makes a chain of screens like screen 1 > 2 > 3 and so on with functionallity to come back to the previous screens (LIFO stack)
-		<Stack />
-	);
+  return (
+    // Stack makes a chain of screens like screen 1 > 2 > 3 and so on with functionallity to come back to the previous screens (LIFO stack)
+    <Stack
+      screenOptions={{
+        headerShadowVisible: false,
+        contentStyle: { backgroundColor: Colors.background }, // sets the background color of the page
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Today",
+          headerLargeTitle: true,
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default Layout;
