c8170753af8ea79ca4823455f578150dc33abd68
Erik Velf <velf.erik@protonmail.com>
2025-04-02 21:45:50 +0200

    Add styles search bar in search tab category

--- app/(authenticated)/(tabs)/search/_layout.tsx
+++ app/(authenticated)/(tabs)/search/_layout.tsx
@@ -1,10 +1,31 @@
 import { Stack } from "expo-router";
+import { Colors } from "@/constants/Colors";
 
 const Layout = () => {
-	return (
-		// Stack makes a chain of screens like screen 1 > 2 > 3 and so on with functionallity to come back to the previous screens (LIFO stack)
-		<Stack />
-	);
+  return (
+    // Stack makes a chain of screens like screen 1 > 2 > 3 and so on with functionallity to come back to the previous screens (LIFO stack)
+    <Stack
+      screenOptions={{
+        headerShadowVisible: false,
+        contentStyle: { backgroundColor: Colors.backgroundAlt },
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          headerLargeTitle: true,
+          title: "Search",
+          headerSearchBarOptions: {
+            placeholder: "Tasks, Projects and More",
+            tintColor: Colors.primary,
+            textColor: Colors.primary,
+            headerIconColor: Colors.primary,
+            barTintColor: Colors.backgroundAlt,
+          },
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default Layout;
