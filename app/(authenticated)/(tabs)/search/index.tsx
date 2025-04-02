7686327cab73ae5edc4e6f8d2c57495f5ecdf30b
Erik Velf <velf.erik@protonmail.com>
2025-04-02 21:40:27 +0200

    Add ScrollView with dummy text in search tab category

--- app/(authenticated)/(tabs)/search/index.tsx
+++ app/(authenticated)/(tabs)/search/index.tsx
@@ -1,11 +1,27 @@
-import { View, Text } from "react-native";
+import { ReactElement } from "react";
+import { View, Text, ScrollView } from "react-native";
+
+const createNumberedText = () => {
+  const arr: Array<number> = [];
+  for (let i = 1; i <= 60; i++) {
+    arr.push(i);
+  }
+
+  const numberedText: Array<ReactElement> = arr.map((num) => {
+    return <Text>Text number {num}</Text>;
+  });
+  return numberedText;
+};
 
 const Page = () => {
-	return (
-		<View>
-			<Text>Search</Text>
-		</View>
-	);
+  return (
+    <ScrollView contentInsetAdjustmentBehavior="automatic">
+      <View>
+        <Text>Search</Text>
+        {createNumberedText()}
+      </View>
+    </ScrollView>
+  );
 };
 
 export default Page;
