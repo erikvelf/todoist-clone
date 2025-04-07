1a5f42a6428dd8a17bed7a6662bad4d932d8db68
Erik Velf <velf.erik@protonmail.com>
2025-04-07 20:59:51 +0200

    Add FAB button to Upcoming tab section

--- app/(authenticated)/(tabs)/upcoming/_layout.tsx
+++ app/(authenticated)/(tabs)/upcoming/_layout.tsx
@@ -1,9 +1,26 @@
+import MoreButton from "@/components/MoreButton";
+import { Colors } from "@/constants/Colors";
 import { Stack } from "expo-router";
 
 const Layout = () => {
 	return (
 		// Stack makes a chain of screens like screen 1 > 2 > 3 and so on with functionallity to come back to the previous screens (LIFO stack)
-		<Stack />
+    <Stack
+      screenOptions={{
+        headerShadowVisible: false,
+        contentStyle: { backgroundColor: Colors.background }, // sets the background color of the page
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Upcoming",
+          headerLargeTitle: true,
+          headerRight: () => <MoreButton />,
+        }}
+      />
+    </Stack>
+
 	);
 };
 
--- app/(authenticated)/(tabs)/upcoming/index.tsx
+++ app/(authenticated)/(tabs)/upcoming/index.tsx
@@ -1,11 +1,19 @@
-import { View, Text } from "react-native";
+import Fab from "@/components/Fab";
+import { View, Text, StyleSheet } from "react-native";
 
 const Page = () => {
 	return (
-		<View>
+		<View style={styles.container}>
 			<Text>Page</Text>
+			<Fab />
 		</View>
 	);
 };
 
 export default Page;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+	},
+});
