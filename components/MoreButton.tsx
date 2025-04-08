a43a64701112507535d9178b7299ffb75a27595f
Erik Velf <velf.erik@protonmail.com>
2025-04-08 22:12:38 +0200

    Add Copy entry in Zeego MoreButton menu

--- components/MoreButton.tsx
+++ components/MoreButton.tsx
@@ -1,10 +1,36 @@
-import { Text, View } from "react-native";
+import { Text, TouchableOpacity, View, ImageSourcePropType } from "react-native";
+import * as DropdownMenu from "zeego/dropdown-menu"
+import { useRouter } from "expo-router";
+import Icon from "@react-native-vector-icons/ionicons";
 
-const MoreButton = () => {
+// Get the icon source and ensure it's properly typed
+const linkIcon: ImageSourcePropType = { uri: Icon.getImageSourceSync("link", 24)?.uri || "" };
+
+type MoreButtonProps = {
+  pageName: string;
+}
+
+const MoreButton = ({ pageName }: MoreButtonProps) => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>MoreButton</Text>
-    </View>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <TouchableOpacity>
+            <Text>{pageName}</Text>
+        </TouchableOpacity>
+      </DropdownMenu.Trigger>
+
+      <DropdownMenu.Content>
+        <DropdownMenu.Item key="link">
+          <DropdownMenu.ItemTitle>Copy</DropdownMenu.ItemTitle>
+          <DropdownMenu.ItemImage
+            source={linkIcon}
+            height={24}
+            width={24}
+          />
+        </DropdownMenu.Item>
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
   );
 };
 
