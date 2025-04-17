97b14a30560475400134798ba6d17fdc9475a01b
Erik Velf <velf.erik@protonmail.com>
2025-04-17 18:25:15 +0200

    Modified metro.config.js to include more detailed error reports

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,9 @@
-const {
-  getSentryExpoConfig
-} = require("@sentry/react-native/metro");
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getSentryExpoConfig(__dirname);
-config.resolver.sourceExts.push('sql');
+const config = getSentryExpoConfig(__dirname, {
+	enableSourceContextInDevelopment: true,
+	annotateReactComponents: true,
+});
+config.resolver.sourceExts.push("sql");
 module.exports = config;
+
