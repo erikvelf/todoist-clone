a56d3cc6a42dc754ee17c5110ecd10ff485e3ab1
Erik Velf <velf.erik@protonmail.com>
2025-04-22 21:54:41 +0200

    Add new-task and task screens bottom sheet

--- app/(authenticated)/_layout.tsx
+++ app/(authenticated)/_layout.tsx
@@ -1,11 +1,37 @@
+import { Colors } from "@/constants/Colors";
 import { Stack } from "expo-router";
+import { useWindowDimensions } from "react-native";
 
 const Layout = () => {
+  const screenHeight = useWindowDimensions().height;
+
   return (
     <Stack screenOptions={{ contentStyle: { backgroundColor: "fff" } }}>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="task/new" options={{presentation: "modal"}} />
-      <Stack.Screen name="task/[id]" options={{presentation: "modal"}} />
+      <Stack.Screen name="task/new" options={{
+        presentation: "formSheet",
+        title: "",
+        headerShown: false,
+        contentStyle: {
+          backgroundColor: Colors.background,
+        },
+        sheetAllowedDetents: screenHeight > 700 ? [0.22] : "fitToContents",
+        sheetGrabberVisible: false,
+        sheetExpandsWhenScrolledToEdge: false,
+        sheetCornerRadius: 10,
+      }} />
+      <Stack.Screen name="task/[id]" options={{
+        presentation: "formSheet",
+        title: "",
+        headerShown: false,
+        contentStyle: {
+          backgroundColor: Colors.background,
+        },
+        sheetAllowedDetents: screenHeight > 700 ? [0.22] : "fitToContents",
+        sheetGrabberVisible: false,
+        sheetExpandsWhenScrolledToEdge: false,
+        sheetCornerRadius: 10,
+      }} />
     </Stack>
   );
 };
