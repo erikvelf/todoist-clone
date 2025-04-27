a05f075c08351a7e3b531c085ac5c1dd46729b38
Erik Velf <velf.erik@protonmail.com>
2025-04-27 21:54:36 +0200

    Fixed dummy data in search tab section

--- app/(authenticated)/(tabs)/search/index.tsx
+++ app/(authenticated)/(tabs)/search/index.tsx
@@ -2,13 +2,13 @@ import { ReactElement } from "react";
 import { View, Text, ScrollView } from "react-native";
 
 const createNumberedText = () => {
-  const arr: Array<number> = [];
+  const arr: number[] = [];
   for (let i = 1; i <= 60; i++) {
     arr.push(i);
   }
 
   const numberedText: Array<ReactElement> = arr.map((num) => {
-    return <Text>Text number {num}</Text>;
+    return <Text key={num}>Text number {num}</Text>;
   });
   return numberedText;
 };
