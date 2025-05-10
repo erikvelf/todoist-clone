f4d778898dc53ada9bb08e55c40977f7053f5ec2
Erik Velf <velf.erik@protonmail.com>
2025-05-10 12:02:50 +0200

    Add additional values for BottomSheet context to open bottomsheet with prefilled data

--- context/BottomSheetContext.tsx
+++ context/BottomSheetContext.tsx
@@ -1,17 +1,27 @@
-import React, { createContext, useRef, useContext, ReactNode } from 'react';
+import React, { createContext, useRef, useContext, ReactNode, useState } from 'react';
 import BottomSheet from '@gorhom/bottom-sheet';
+import { Todo } from '@/types/interfaces';
 
 interface BottomSheetContextType {
   bottomSheetRef: React.RefObject<BottomSheet>;
+  openBottomSheetWithTask: (task: Todo) => void; // open the bottom sheet with a task
+  selectedTask: Todo | null; // State to hold the task for the bottom sheet
 }
 
 const BottomSheetContext = createContext<BottomSheetContextType | undefined>(undefined);
 
 export const BottomSheetProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   const bottomSheetRef = useRef<BottomSheet>(null);
+  const [selectedTask, setSelectedTask] = useState<Todo | null>(null); // State for the task to be displayed in the bottom sheet
+
+  // Function to open the bottom sheet with a task
+ const openBottomSheetWithTask = (task: Todo) => {
+    setSelectedTask(task);
+    bottomSheetRef.current?.expand();
+  };
 
   return (
-    <BottomSheetContext.Provider value={{ bottomSheetRef }}>
+    <BottomSheetContext.Provider value={{ bottomSheetRef, openBottomSheetWithTask, selectedTask }}>
       {children}
     </BottomSheetContext.Provider>
   );
