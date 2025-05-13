f4f2359167516c5e16eb6be10dcf5004395d03c7
Erik Velf <velf.erik@protonmail.com>
2025-05-13 17:30:03 +0200

    Add basic calendar

--- app/(authenticated)/(tabs)/upcoming/index.tsx
+++ app/(authenticated)/(tabs)/upcoming/index.tsx
@@ -1,19 +1,78 @@
 import Fab from "@/components/Fab";
-import { View, Text, StyleSheet } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import React, { useCallback } from "react";
+import { useSQLiteContext } from "expo-sqlite";
+import { drizzle, useLiveQuery } from "drizzle-orm/expo-sqlite";
+import { eq } from "drizzle-orm";
+import { projects, todos } from "@/db/schema";
+import { format } from "date-fns";
+import { Todo } from "@/types/interfaces";
+import { useEffect, useState } from "react";
+import { Colors } from "@/constants/Colors";
+import TaskRow from "@/components/TaskRow";
+import { Calendar, toLocaleDateString } from "@fowusu/calendar-kit";
+import { CalendarMonthName, CustomDayComponent, WeekDayNameComponent } from "@/components/CalendarComponents";
+
+interface Section {
+	title: string;
+	data: Todo[];
+}
+
+const today = new Date();
+const todayDateString = toLocaleDateString(today);
+
+
 
 const Page = () => {
-	return (
-		<View style={styles.container}>
-			<Text>Page</Text>
-			<Fab />
-		</View>
-	);
+  const [selectedDay, setSelectedDay] = useState<string>(todayDateString);
+
+  const onDayPress = useCallback((dateString: string) => {
+    setSelectedDay(dateString);
+  }, []);
+
+	const db = useSQLiteContext();
+	const drizzleDb = drizzle(db);
+
+	const today = new Date().toISOString();
+
+	const { data } = useLiveQuery(
+		drizzleDb
+			.select()
+			.from(todos)
+			.leftJoin(projects, eq(todos.project_id, projects.id)).
+			where(eq(todos.completed, 0))
+	)
+
+  return (
+    <>
+      {/* <Fab /> */}
+     <Calendar
+      dayContainerStyle={{ backgroundColor: 'transparent' }}
+      contentContainerStyle={{ backgroundColor: Colors.background, borderRadius: 12, padding: 8 }}
+      weeksContainerStyle={{ backgroundColor: Colors.background }}
+      MonthNameComponent={CalendarMonthName}
+      WeekDayNameComponent={WeekDayNameComponent}
+      DayComponent={CustomDayComponent}
+      date={todayDateString}
+      markedDates={[selectedDay]}
+      onDayPress={onDayPress}
+    />
+   </>
+  );
 };
 
 export default Page;
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
+    backgroundColor: 'red',
+    // justifyContent: 'center',
+    // alignItems: 'center',
 	},
+  calendarContainer: {
+    backgroundColor: Colors.background,
+  },
+  dayContainer: {
+    backgroundColor: Colors.primary,
+  }
 });
