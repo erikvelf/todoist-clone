dbb13479ee75ba42ce08f4e90837588746e20c74
Erik Velf <velf.erik@protonmail.com>
2025-05-14 22:36:23 +0200

    Cleaned up android specific code in MoreButton

--- components/MoreButton.tsx
+++ components/MoreButton.tsx
@@ -1,7 +1,5 @@
-import { Text, TouchableOpacity, View, ImageSourcePropType } from "react-native";
+import { TouchableOpacity } from "react-native";
 import * as DropdownMenu from "zeego/dropdown-menu"
-import { useRouter } from "expo-router";
-import Icon from "@react-native-vector-icons/ionicons";
 import * as Clipboard from "expo-clipboard"
 import { toast } from "sonner-native";
 import { Ionicons } from "@expo/vector-icons";
@@ -39,8 +37,6 @@ type MoreButtonProps = {
 }
 
 const MoreButton = ({ pageName }: MoreButtonProps) => {
-  const router = useRouter();
-
   const copyToClipboard = () => {
     const path = `eriktodos://(authenticated)/(tabs)/${pageName.toLowerCase()}`;
     Clipboard.setStringAsync(path)
@@ -51,55 +47,29 @@ const MoreButton = ({ pageName }: MoreButtonProps) => {
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
         <TouchableOpacity>
-          <Ionicons name="ellipsis-horizontal-outline" size={30} color={Colors.primary}/>
+          <Ionicons name="ellipsis-horizontal-outline" size={30} color={Colors.primary} />
         </TouchableOpacity>
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Content>
         <DropdownMenu.Item key="link" onSelect={copyToClipboard}>
           <DropdownMenu.ItemTitle>Copy</DropdownMenu.ItemTitle>
-          <DropdownMenu.ItemImage
-            source={{ uri: Icon.getImageSourceSync("copy", 24)?.uri || "" }}
-            height={24}
-            width={24}
-          />
         </DropdownMenu.Item>
 
+        <DropdownMenu.Group>
 
-      <DropdownMenu.Group>
           <DropdownMenu.Item key="select">
             <DropdownMenu.ItemTitle>Select Task</DropdownMenu.ItemTitle>
-            <DropdownMenu.ItemImage
-              source={{ uri: Icon.getImageSourceSync("albums-outline", 24)?.uri || "" }}
-              height={24}
-              width={24}
-            />
-            <DropdownMenu.ItemIcon androidIconName="sym_action_email" />
           </DropdownMenu.Item>
 
           <DropdownMenu.Item key="view">
             <DropdownMenu.ItemTitle>View</DropdownMenu.ItemTitle>
-            <DropdownMenu.ItemImage
-              source={{ uri: Icon.getImageSourceSync("eye", 24)?.uri || "" }}
-              height={24}
-              width={24}
-            />
-          <DropdownMenu.ItemIcon androidIconName="ic_menu_view" />
-        </DropdownMenu.Item>
+          </DropdownMenu.Item>
 
           <DropdownMenu.Item key="activity">
             <DropdownMenu.ItemTitle>Activity Log</DropdownMenu.ItemTitle>
-            <DropdownMenu.ItemImage
-              source={{ uri: Icon.getImageSourceSync("time", 24)?.uri || "" }}
-              height={24}
-              width={24}
-            />
-            {/* <DropdownMenu.ItemIcon 
-              androidIconName="ic_menu_recent_history"
-              style={{ color: Colors.dark }}
-            /> */}
-            
           </DropdownMenu.Item>
+
         </DropdownMenu.Group>
       </DropdownMenu.Content>
     </DropdownMenu.Root>
