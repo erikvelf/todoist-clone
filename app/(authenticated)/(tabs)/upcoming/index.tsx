69f9f1baa904fc1fd28bd9df09bba7fe1e24ebe4
Erik Velf <velf.erik@protonmail.com>
2025-05-15 21:58:34 +0200

    Add tasks list in upcoming page

--- app/(authenticated)/(tabs)/upcoming/index.tsx
+++ app/(authenticated)/(tabs)/upcoming/index.tsx
@@ -1,5 +1,5 @@
 import Fab from "@/components/Fab";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, SectionList } from "react-native";
 import React, { useCallback } from "react";
 import { useSQLiteContext } from "expo-sqlite";
 import { drizzle, useLiveQuery } from "drizzle-orm/expo-sqlite";
@@ -21,14 +21,12 @@ interface Section {
 const today = new Date();
 const todayDateString = toLocaleDateString(today);
 
-
-
 const Page = () => {
-  const [selectedDay, setSelectedDay] = useState<string>(todayDateString);
+	const [selectedDay, setSelectedDay] = useState<string>(todayDateString);
 
-  const onDayPress = useCallback((dateString: string) => {
-    setSelectedDay(dateString);
-  }, []);
+	const onDayPress = useCallback((dateString: string) => {
+		setSelectedDay(dateString);
+	}, []);
 
 	const db = useSQLiteContext();
 	const drizzleDb = drizzle(db);
@@ -43,36 +41,80 @@ const Page = () => {
 			where(eq(todos.completed, 0))
 	)
 
-  return (
-    <>
-      {/* <Fab /> */}
-     <Calendar
-      dayContainerStyle={{ backgroundColor: 'transparent' }}
-      contentContainerStyle={{ backgroundColor: Colors.background, borderRadius: 12, padding: 8 }}
-      weeksContainerStyle={{ backgroundColor: Colors.background }}
-      MonthNameComponent={CalendarMonthName}
-      WeekDayNameComponent={WeekDayNameComponent}
-      DayComponent={CustomDayComponent}
-      date={todayDateString}
-      markedDates={[selectedDay]}
-      onDayPress={onDayPress}
-    />
-   </>
-  );
+	const [sectionListData, setSectionListData] = useState<Section[]>([]);
+
+  useEffect(() => {
+    const formattedData: Todo[] = data?.map((item) => ({
+      ...item.todos,
+      project_name: item.projects?.name,
+      project_color: item.projects?.color,
+    }));
+
+    const groupedByDay = formattedData?.reduce((acc: { [key: string]: Todo[] }, task) => {
+      // format the date to be like this: 15 April · Mon
+      const day = format(task.due_date || new Date(), 'd MMM · eee');
+      if (!acc[day]) {
+        acc[day] = [];
+      }
+      acc[day].push(task);
+      return acc;
+    }, {})
+
+    const listData: Section[] = Object.entries(groupedByDay || {}).map(([day, tasks]) => ({
+      title: day,
+      data: tasks,
+    }));
+
+    // Sort sections by date
+    listData.sort((a, b) => {
+      const dateA = new Date(a.data[0].due_date || new Date());
+      const dateB = new Date(b.data[0].due_date || new Date());
+      return dateA.getTime() - dateB.getTime();
+    });
+    console.log("Page ~ listData sorted", listData);
+    console.log(JSON.stringify(listData, null, 2));
+
+    setSectionListData(listData);
+
+  }, [data]);
+
+
+	return (
+		<>
+			<Fab />
+			<Calendar
+				dayContainerStyle={{ backgroundColor: 'transparent' }}
+				contentContainerStyle={{ backgroundColor: Colors.background, borderRadius: 12, padding: 8 }}
+				weeksContainerStyle={{ backgroundColor: Colors.background }}
+				MonthNameComponent={CalendarMonthName}
+				WeekDayNameComponent={WeekDayNameComponent}
+				DayComponent={CustomDayComponent}
+				date={todayDateString}
+				markedDates={[selectedDay]}
+				onDayPress={onDayPress}
+				viewAs="week"
+			/>
+      <SectionList
+        sections={sectionListData}
+        renderItem={({ item }) => <TaskRow task={item} />}
+        renderSectionHeader={({ section }) => <Text>{section.title}</Text>}
+      />
+		</>
+	);
 };
 
 export default Page;
 
 const styles = StyleSheet.create({
 	container: {
-    backgroundColor: 'red',
-    // justifyContent: 'center',
-    // alignItems: 'center',
+		backgroundColor: 'red',
+		// justifyContent: 'center',
+		// alignItems: 'center',
 	},
-  calendarContainer: {
-    backgroundColor: Colors.background,
-  },
-  dayContainer: {
-    backgroundColor: Colors.primary,
-  }
+	calendarContainer: {
+		backgroundColor: Colors.background,
+	},
+	dayContainer: {
+		backgroundColor: Colors.primary,
+	}
 });
