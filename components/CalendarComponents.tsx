7f84f842b704f20529d045cdb2fc09786eada096
Erik Velf <velf.erik@protonmail.com>
2025-05-15 21:56:26 +0200

    Styled CalendarComponents

--- components/CalendarComponents.tsx
+++ components/CalendarComponents.tsx
@@ -1,5 +1,5 @@
 import { Colors } from "@/constants/Colors";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 import type { InnerDayProps } from "@fowusu/calendar-kit";
 
@@ -9,8 +9,8 @@ export interface CalendarMonthNameProps {
 }
 
 export const CalendarMonthName: React.FC<CalendarMonthNameProps> = ({ month, locale }) => (
-  <View style={{ backgroundColor: Colors.primary, paddingVertical: 8, borderRadius: 8, marginBottom: 4 }}>
-    <Text style={{ color: '#fff', fontWeight: 'bold', fontSize: 18, textAlign: 'center' }}>
+  <View style={{ paddingVertical: 8, borderRadius: 8, marginBottom: 4 }}>
+    <Text style={{ color: Colors.dark, fontWeight: 'bold', fontSize: 18, textAlign: 'center' }}>
       {month.toLocaleDateString(locale || 'en-US', { month: 'long', year: 'numeric' })}
     </Text>
   </View>
@@ -21,38 +21,96 @@ export interface WeekDayNameComponentProps {
 }
 
 export const WeekDayNameComponent: React.FC<WeekDayNameComponentProps> = ({ weekDays }) => (
-  <View style={{ flexDirection: 'row', backgroundColor: Colors.primary, borderRadius: 8, marginBottom: 4 }}>
+  <View style={{ flexDirection: 'row', borderRadius: 8, marginBottom: 4 }}>
     {weekDays.map((day, idx) => (
       <View key={day + idx} style={{ flex: 1, alignItems: 'center', paddingVertical: 4 }}>
-        <Text style={{ color: '#fff', fontWeight: '600' }}>{day}</Text>
+        <Text style={{ color: Colors.lightText, fontWeight: '600' }}>{day}</Text>
       </View>
     ))}
   </View>
 );
 
-export const CustomDayComponent: React.FC<InnerDayProps<Record<string, unknown>>> = ({ day, isSelected, isToday, state, locale }) => {
-  // const isToday = day.toDateString() === today.toDateString();
-  const backgroundColor = isSelected 
-    ? Colors.primary 
-    : isToday 
-      ? '#FF00004D' // Fainted red for today
-      : 'transparent';
+export const CustomDayComponent: React.FC<InnerDayProps<Record<string, unknown>>> = ({ 
+  day, 
+  isSelected, 
+  isToday, 
+  state, 
+  locale 
+}) => {
+  let containerStyle = [styles.dayContainer];
+  let textStyle = [styles.dayText];
 
+  if (isSelected) {
+    containerStyle.push(styles.selectedDayContainer);
+    textStyle.push(styles.selectedDayText);
+  } else if (isToday) {
+    textStyle.push(styles.todayText);
+  }
 
   return (
-    <View style={{
-      backgroundColor,
-      borderRadius: 8,
-      alignItems: 'center',
-      justifyContent: 'center',
-      margin: 2,
-      aspectRatio: 1,
-      minWidth: 36,
-      minHeight: 36,
-    }}>
-      <Text style={{ color: isSelected ? '#fff' : Colors.dark, fontWeight: isSelected ? 'bold' : 'normal', fontSize: 16 }}>
+    <View style={containerStyle}>
+      <Text style={textStyle}>
         {day.toLocaleDateString(locale || 'en-US', { day: 'numeric' })}
       </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  dot: {
+    backgroundColor: Colors.dark,
+    width: 4,
+    height: 4,
+    borderRadius: 2,
+    marginTop: 2,
+  },
+  dotRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 2,
+    borderWidth: 1,
+    borderColor: 'red',
+  },
+  dotCount: {
+    fontSize: 10,
+    color: Colors.primary,
+    marginLeft: 2,
+    marginTop: -2,
+    fontWeight: 'bold',
+    width: 16,
+    height: 16,
+  },
+  dayContainer: {
+    borderRadius: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 2,
+    aspectRatio: 1,
+    minWidth: 36,
+    minHeight: 36,
+    backgroundColor: 'transparent',
+  },
+  selectedDayContainer: {
+    borderRadius: 80,
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 2,
+    aspectRatio: 1,
+    minWidth: 36,
+    minHeight: 36,
+    backgroundColor: Colors.primary,
+  },
+  dayText: {
+    fontSize: 16,
+  },
+  selectedDayText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+  todayText: {
+    color: Colors.primary,
+    fontWeight: 'bold',
+    fontSize: 16,
+  }
+})
